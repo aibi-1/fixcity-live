69df95cf500b48cb07507f95e88d693a96ef00fc
aibi-1 <ajbi2656@gmail.com>
2026-05-02 15:03:37 +0600

    fix

--- my_project/app.py
+++ my_project/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    result = None
+    if request.method == "POST":
+        user_input = request.form.get("text") # в HTML name="text"
+        result = f"Ты ввел: {user_input}"
+    return render_template("index.html", result=result)
+
+@app.route('/map')
+def map_page():
+    return render_template('map.html')
+
+@app.route('/news')
+def news_page(): # изменил имя
+    return render_template('news.html')
+
+@app.route('/route') # изменил путь с /ro на /route
+def route_page(): # изменил имя
+    return render_template('route.html')
+
+@app.route('/complaints')
+def complaint_page():
+    return render_template('complaint.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
